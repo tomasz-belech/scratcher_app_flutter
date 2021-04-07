b6b016a1fa4e4776e238d4778bef8455fe472037
Tomasz Belech <tomaszbelech1@gmail.com>
2021-04-07 13:11:36 +0200

    admin panel added

--- scratcher_flutter/lib/firebaseDatabase.dart
+++ scratcher_flutter/lib/firebaseDatabase.dart
@@ -1,27 +1,29 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseService {
-//collection references
   final CollectionReference usersCollection =
       FirebaseFirestore.instance.collection('users');
 
   Future addUserData(String email, bool won) async {
-    return await usersCollection.doc().set({
-      'email': email,
-      'won': won,
-      'dateTime' : DateTime.now()
-    });
+    return await usersCollection
+        .doc()
+        .set({'email': email, 'won': won, 'dateTime': DateTime.now()});
+  }
+
+  Future getUserList() async {
+    List itemsList = [];
+
+    try {
+      await usersCollection.get().then((querySnapshot) {
+        querySnapshot.docs.forEach((element) {
+          element.id;
+          itemsList.add(element.data());
+        });
+      });
+      return itemsList;
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
   }
 }
-
-//   Future getUserList() async {
-//     try {
-//       await usersCollection.getDocuments().then((querySnapshot) {
-//         querySnapshot.documents.forEach((element) {});
-//       });
-//     } catch (e) {
-//       print(e.toString());
-//       return null;
-//     }
-//   }
-// }
--- scratcher_flutter/lib/loggedWiew.dart
+++ scratcher_flutter/lib/loggedWiew.dart
@@ -1,6 +1,5 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:scratcher_flutter/firebaseDatabase.dart';
 
 class LoggedWiew extends StatefulWidget {
   @override
@@ -8,11 +7,43 @@ class LoggedWiew extends StatefulWidget {
 }
 
 class _LoggedWiewState extends State<LoggedWiew> {
+  List userProfileList = [];
+
+  void initState() {
+    super.initState();
+    fetchDatabaseList();
+  }
+
+  fetchDatabaseList() async {
+    dynamic resultant = await DatabaseService().getUserList();  
+
+    if (resultant == null) {
+      print('Unable to retrieve');
+    } else {
+      setState(() {
+        userProfileList = resultant;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Text('History Wiew'),
-      
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        child: ListView.builder(
+          itemCount: userProfileList.length,
+          itemBuilder: (context, index) {
+            return Card(
+              child: ListTile(
+                title: Text("Email: ${userProfileList[index]['email']}"),
+                subtitle: Text("Date: ${userProfileList[index]['dateTime']}"),
+                trailing: Text("Won :'${userProfileList[index]['won']}"),
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
